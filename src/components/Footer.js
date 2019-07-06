058a9aeccc4745cbbbc7ed428002d71055331726
Dan Lawson <dan@propandpose.co>
2019-07-06 16:53:21 +1000

    Footer Text Addition

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,17 +26,17 @@ const Footer = class extends React.Component {
                   <ul className="menu-list">
                     <li>
                       <Link to="/" className="navbar-item">
-                        Home
+                        Home Page
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/about">
-                        About
+                        About Page
                       </Link>
                     </li>
                     <li>
                       <Link className="navbar-item" to="/products">
-                        Products
+                        Products Page
                       </Link>
                     </li>
                     <li>
